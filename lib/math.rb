1ce3e2188a7dbe99d7dfbacf75e460a0ee7e6703
Alexander kagerer <kagerer19@gmail.com>
2019-11-15 16:56:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,22 +1,23 @@
 def addition(num1, num2)
-
+ sum = (num1 + num2)
 end
 
 def subtraction(num1, num2)
-
+  sub = (num1 - num2)
 end
 
 def division(num1, num2)
-
+   puts = (num1 / num2)
 end
 
 def multiplication(num1, num2)
-
+ puts = (num1 * num2)
 end
 
 def modulo(num1, num2)
-
+  math.modulo = (num2 % num1)
 end
+
 
 def square_root(num)
 
